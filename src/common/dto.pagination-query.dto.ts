a628b47084ab678d9cf8d9eedbf1206d60744d83
nati-elmaliach <natielmaliach3197@gmail.com>
2022-02-20 23:30:42 +0200

    base nestjs

---
+++ src/common/dto.pagination-query.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from "class-transformer";
+import { IsOptional, IsPositive } from "class-validator";
+
+/* PaginationQueryDto */
+export class PaginationQueryDto {
+  
+  @IsOptional()
+  @IsPositive()
+  limit: number;
+
+  
+  @IsOptional()
+  @IsPositive()
+  offset: number;
+}
